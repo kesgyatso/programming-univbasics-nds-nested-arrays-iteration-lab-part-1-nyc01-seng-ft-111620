b4fe4bbd873c6b7befb64c09276e2e30f3c120e9
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-11-10 06:16:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,20 +5,15 @@ array_of_arrays =[
   ]
 
 def find_even_values(src)
- 
-row_index=50
-
+  row_index = 0
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
       if src[row_index][element_index].even?
         p src[row_index][element_index]
       end
-    element_index +=1
+      element_index += 1
     end
-    
-    row_index +=1
+    row_index += 1
   end
 end
-
-find_even_values(array_of_arrays)
