bf7c1333e42437122f8d4370fb3b91bbac4303ca
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-11-10 06:15:52 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -13,9 +13,11 @@ row_index=50
 
   while row_index < src.count do
     element_index = 0
-    while element_index <
-    src[row_index].count do
-      element_index +=1
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].even?
+        p src[row_index][element_index]
+      end
+    element_index +=1
     end
     
     row_index +=1
